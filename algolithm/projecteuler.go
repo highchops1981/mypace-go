867e53e95ad39eb719b46500953bcce67e18825a
keisuke Ishikura <highchops1981@gmail.com>
2018-03-12 17:30:40 +0800

    Update projecteuler.go

--- algolithm/projecteuler.go
+++ algolithm/projecteuler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+//1
 func main() {
 
 	base1 := 3
@@ -24,4 +25,43 @@ func main() {
 	}
 
 	fmt.Println(sum)
+}
+
+//2
+func main() {
+	
+	max := 4000000
+	sum := 0
+	kou := 0
+	const n1 = 1
+	const n2 = 2
+	lastKou := 0
+	lastlastKou := 0
+	
+	for kou <= max {
+	
+		if kou == 0 {
+			
+			kou = n1 + n2
+			lastlastKou = n2
+			lastKou = kou
+		
+		} else {
+		
+			kou = lastlastKou + lastKou
+			lastlastKou = lastKou
+			lastKou = kou
+		
+		}
+		
+		if kou % 2 == 0 {
+		
+			sum += kou
+		}
+	
+	}
+	
+	sum += n2
+	fmt.Println(sum)
+		
 }
